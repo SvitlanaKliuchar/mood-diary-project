dbbd985b93ca933fc37e92dc13fcf8e64f12cf07
SvitlanaKliuchar <svitlanakliuchar@gmail.com>
2025-01-13 23:44:18 +0200

    feat(footer):implement responsive footer

--- client/src/components/landing/Footer.jsx
+++ client/src/components/landing/Footer.jsx
@@ -3,9 +3,28 @@ import styles from './Landing.module.css'
 
 const Footer = () => {
   return (
-    <div>
-      /* Your JSX here */
-    </div>
+    <footer>
+      <div className={styles['footer-content']}>
+        <div className={styles['footer-links']}>
+          <div>
+            <h3>Support</h3>
+            <a href="#">Contact us</a>
+            <a href="#">FAQ</a>
+          </div>
+          <div>
+            <h3>About us</h3>
+            <a href="#">About</a>
+            <a href="#">Guide</a>
+          </div>
+          <div>
+            <h3>Legal</h3>
+            <a href="#">Terms</a>
+            <a href="#">Privacy</a>
+          </div>
+        </div>
+        <p>&copy; 2025 Harmonia. All rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
