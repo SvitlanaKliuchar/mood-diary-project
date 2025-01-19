0b942088509b1c668130e4a3700ec62a4c98787e
SvitlanaKliuchar <svitlanakliuchar@gmail.com>
2025-01-19 02:24:01 +0200

    feat(home): build a responsive Entries component with hardcoded mood entries

--- client/src/components/home/Entries.jsx
+++ client/src/components/home/Entries.jsx
@@ -1,11 +1,260 @@
 import React from 'react';
+import styles from './Home.module.css'
 
 const Entries = () => {
-  return (
-    <div>
-      /* Your JSX here */
-    </div>
-  );
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles['all-entries-container']}>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                    
+
+                </ul>
+            </div>
+
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+            <div className={styles['entry-container']}> {/* another grid */}
+                <div className={styles.mood}>
+                    <img className={styles.emoji} src="src/assets/icons/moods/great.svg" alt="extremely happy emoji, representing great mood" />
+                    <span>great</span>
+                </div>
+                <div className={styles['date-time']}>
+                    <h3 className={styles.date}>yesterday, 2 jan</h3>
+                    <div className={styles.time}>22:34</div>
+                </div>
+                <ul className={styles['mood-tags']}>
+                    <li className={styles['mood-tag']}>happy</li>
+                    <li className={styles['mood-tag']}>grateful</li>
+                </ul>
+            </div>
+        </div>
+        </div>
+        
+    );
 };
 
 export default Entries;
