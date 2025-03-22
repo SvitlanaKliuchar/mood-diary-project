b95ca92d4bec82d75b01b14720cc387a59160297
SvitlanaKliuchar <svitlanakliuchar@gmail.com>
2025-03-23 01:42:30 +0200

    feat(Settings): implement skeleton UI

---
+++ client/src/components/settings/SettingsList.jsx
@@ -0,0 +1,64 @@
+import styles from './SettingsList.module.css'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const SettingsList = () => {
+    const [darkMode, setDarkMode] = useState(false)
+
+    const handleToggle = (e) => {
+        setDarkMode(e.target.checked)
+        //update app theme context
+    }
+
+    return (
+        <nav className={styles['settings-container']} aria-label='Settings Navigation'>
+            <ul className={styles['settings-list']}>
+                <li>
+                    <h2>Profile & Account Management</h2>
+                    <ul className={styles.profile}>
+                        <li>
+                            {/* (Name, avatar, email, change password) */}
+                            <Link to="/profile">Personal Info</Link>
+                        </li>
+                        <li>
+                            <Link to="/account/delete">Delete Account</Link>
+                        </li>
+                        <li>
+                            <Link to="/account/export">Export/Import Data</Link>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <h2>Theme & Appearance</h2>
+                    <div className={styles['toggle-container']}>
+                        <label htmlFor="dark-mode-toggle" className={styles['toggle-label']}>Dark Mode
+                            <input
+                                type="checkbox"
+                                id='dark-mode-toggle'
+                                name='dark-mode-toggle'
+                                checked={darkMode}
+                                onChange={handleToggle}
+                                className={styles['toggle-input']}
+                            />
+                            <span className={styles['toggle-switch']}></span>
+                        </label>
+                    </div>
+                </li>
+                <li>
+                    <h2>Notifications & Reminders</h2>
+                    <ul className={styles.notifications}>
+                        <li>
+                            <Link to="/notifications/push">Push Notifications</Link>
+                        </li>
+                        <li>
+                            <Link to="/notifications/reminders">Custom Reminder Times</Link>
+                        </li>
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
+    );
+};
+
+export default SettingsList;
--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -1,12 +1,12 @@
 import NavBar from "../components/landing/NavBar.jsx";
-
+import SettingsList from "../components/settings/SettingsList.jsx";
 import Footer from "../components/landing/Footer.jsx";
 
 const Login = () => {
   return (
     <>
       <NavBar />
-
+      <SettingsList />
       <Footer />
     </>
   );
